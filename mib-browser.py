d2e13d74589f551898f91686a1ab1f112205269a
Hank <hz4154261@gmail.com>
2022-09-01 16:28:23 +0800

    fix

--- mib-browser.py
+++ mib-browser.py
@@ -4,13 +4,16 @@ from xml.dom import minidom
 
 SwitchIP = input('Switch IP :')
 SNMPVersion = input('SNMP Version 1 or 2 or 3 : ')
+if not SNMPVersion : SNMPVersion = '2' #Default SNMPv2
 DefaultOID = input('default OID ex .1.3 : ')
+if not DefaultOID : DefaultOID = '.1.3' #Default .1.3
 SNMPv3user,SNMPv3Auth,SNMPv3Privacy =None,None,None
-AuthProtocol = usmHMACMD5AuthProtocol
-PrivacyProtocol = usmDESPrivProtocol
+AuthProtocol = usmHMACMD5AuthProtocol #it's Default is MD5
+PrivacyProtocol = usmDESPrivProtocol #it's Default is DES
 
 if SNMPVersion == '1':
     CommunityString = input('Community String :')
+    if not CommunityString : CommunityString = 'public' #Default community Str is public
     iterator = nextCmd(
     SnmpEngine(),
     CommunityData(CommunityString, mpModel=0),
@@ -24,16 +27,16 @@ elif SNMPVersion == '3':
     if isauth == 'y' or isauth == 'Y' :
         SNMPv3Auth = input('Auth Key :')
         Authoption = input('1 = SHA\nNone = MD5\nAuth protocol :')
-        if Authoption == '1' : AuthProtocol = usmHMACSHAAuthProtocol
+        if Authoption == '1' : AuthProtocol = usmHMACSHAAuthProtocol # SHA
 
         isprivacy = input('need privacy y/n ?')
         if isprivacy == 'y' or isprivacy == 'Y':
             SNMPv3Privacy = input('Privacy Key :')
             Privacyoption = input('1 = AES128\n2 = AES192\n3 = AES256\n4=3DES\nNone=DES\nPrivacy protocol : ')
-            if Privacyoption == '1': PrivacyProtocol = usmAesCfb128Protocol
-            elif Privacyoption == '2': PrivacyProtocol = usmAesCfb192Protocol
-            elif Privacyoption == '3': PrivacyProtocol = usmAesCfb256Protocol
-            elif Privacyoption == '4': PrivacyProtocol = usm3DESEDEPrivProtocol
+            if Privacyoption == '1': PrivacyProtocol = usmAesCfb128Protocol #AES128
+            elif Privacyoption == '2': PrivacyProtocol = usmAesCfb192Protocol #AES192
+            elif Privacyoption == '3': PrivacyProtocol = usmAesCfb256Protocol #AES256
+            elif Privacyoption == '4': PrivacyProtocol = usm3DESEDEPrivProtocol #3DES
 
     if SNMPv3Auth and SNMPv3Privacy:
         iterator = nextCmd(
@@ -62,6 +65,7 @@ elif SNMPVersion == '3':
         ObjectType(ObjectIdentity(DefaultOID)))
 else:
     CommunityString = input('Community String :')
+    if not CommunityString : CommunityString = 'public' #Default community Str is public
     iterator = nextCmd(
     SnmpEngine(),
     CommunityData(CommunityString),
